541cd70a66e9fb18ac163c2819dcac0abd62ccb4
Christopher Weyand <christopher.weyand@student.hpi.de>
2018-08-02 17:41:05 +0200

    results

--- plot_metrics/main.py
+++ plot_metrics/main.py
@@ -17,4 +17,14 @@ if __name__ == '__main__':
 
     all_data = analyze(directory, use_cache=True)
 
-    plot(all_data, "pseudo_diameter", directory, False)
+    plot(all_data, "edges", directory, True)
+    plot(all_data, "rounds", directory)
+    plot(all_data, "max_degree", directory, True)
+    plot(all_data, "avg_degree", directory)
+    plot(all_data, "min_degree", directory)
+    plot(all_data, "local_clustering", directory)
+    plot(all_data, "global_clustering", directory)
+    plot(all_data, "pseudo_diameter", directory)
+    plot(all_data, "exponent", directory)
+    plot(all_data, "KS", directory)
+    plot(all_data, "x_min", directory)
